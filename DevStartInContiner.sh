74d0027559e7f15507edaee31143221b1b7261dc
Yousef Amar <yousefamar@gmail.com>
2017-02-16 17:04:53 +0000

    Fix resolution of local registry in dev mode

--- DevStartInContiner.sh
+++ DevStartInContiner.sh
@@ -5,12 +5,12 @@ docker rm databox-cm
 
 docker create \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-        -v `pwd`:/cm \
-        --name databox-cm \
+	-v `pwd`:/cm \
+	--name databox-cm \
 	--label databox.type=container-manager \
-        -e "DATABOX_DEV=1" \
+	-e "DATABOX_DEV=1" \
 	-p 8989:8989 \
-        -t node:latest npm --prefix /cm start
+	-t node:latest npm --prefix /cm start
 
 docker network connect databox-cloud-net databox-cm
 docker network connect databox-app-net databox-cm
