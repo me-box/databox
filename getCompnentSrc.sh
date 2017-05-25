5197c4bc8b4cf30020c89bd5fb9ed22b5e1aae81
ToshBrown <Toshbrown@gmail.com>
2017-05-25 11:37:38 +0100

    Move to master branches

--- getCompnentSrc.sh
+++ getCompnentSrc.sh
@@ -1,34 +1,34 @@
 
 
 if [ ! -d "databox-cm" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-cm.git databox-cm
+    git clone git@github.com:me-box/databox-cm.git databox-cm
 fi 
 
 if [ ! -d "databox-arbiter" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-arbiter.git
+    git clone git@github.com:me-box/databox-arbiter.git
 fi
 
 if [ ! -d "databox-logstore" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-logstore.git
+    git clone git@github.com:me-box/databox-logstore.git
 fi
 
 if [ ! -d "databox-export-service" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-export-service.git
+    git clone git@github.com:me-box/databox-export-service.git
 fi
 
 if [ ! -d "databox-app-server" ]; then
-    git clone  -b fet/swarm git@github.com:me-box/databox-app-server.git
+    git clone  git@github.com:me-box/databox-app-server.git
 fi
 
 if [ ! -d "databox-store-blob-mongo" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-store-blob-mongo.git
+    git clone git@github.com:me-box/databox-store-blob-mongo.git
 fi
 
 if [ ! -d "databox-store-blob" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-store-blob.git
+    git clone git@github.com:me-box/databox-store-blob.git
 fi
 
 if [ ! -d "databox-os-monitor-driver" ]; then
-    git clone -b fet/swarm git@github.com:me-box/databox-os-monitor-driver.git
+    git clone git@github.com:me-box/databox-os-monitor-driver.git
 fi
 
