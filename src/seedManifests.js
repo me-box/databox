68b51b7e3d01dbdccf75aeb392a251d20cd68546
ToshBrown <Toshbrown@gmail.com>
2017-07-14 16:40:21 +0100

    Re-enable pulling from docker hub
    fixes #54

--- src/seedManifests.js
+++ src/seedManifests.js
@@ -20,47 +20,72 @@ const wait = ()=> {
 	});
 }
 
-	wait()
-	.then(()=>{
-		
-			const req = request.defaults({jar: true});
-			req.get("http://" + Config.localAppStoreName,(error,response,body)=>{
-				if(error) {
-					console.log("[seeding manifest] get app store root to log in", error);
-					return Promise.reject();
-				}
-				proms = Config.localAppStoreSeedManifests.map((name)=>{
-					return new Promise(function(resolve, reject) {
-						
-						let path = "./"+name+"/databox-manifest.json";
-						fs.readFile(path,'utf8', (error,body)=>{
-							if(error) {
-								console.log("[seeding manifest] Failed to get manifest from " + path, error);
-							}
-							req.post({
-									uri: "http://" + Config.localAppStoreName + "/app/post",
-									method: "POST",
-									form: {"manifest": body}
-									}, (error,response,body) => {
-										if(error) {
-											console.log("[seeding manifest] Failed to POST manifest " + url, error);
-										} 
-										resolve();
-									});
-						});
-					});
+const seedFromGithub = (name)=> {
+			return new Promise(function(resolve, reject) {
+				url = "https://raw.githubusercontent.com/me-box/"+name+"/master/databox-manifest.json"; 
+				req.get(url,(error,response,body)=>{
+					if(error) {
+						console.log("[seeding manifest] Failed to get manifest from" + url, error);
+					}
+					req.post({
+							uri: "http://" + Config.localAppStoreName + "/app/post",
+							method: "POST",
+							form: {"manifest": body}
+							}, (error,response,body) => {
+								if(error) {
+									console.log("[seeding manifest] Failed to POST manifest " + url, error);
+								} 
+								resolve();
+							});
 				});
-				return Promise.all(proms);
 			});
-		
-	})
-	.then(()=>{
-		console.log("Done!");
-	})
-		
-	.catch(err => {
-		console.log(err);
-		const stack = new Error().stack;
-		console.log(stack);
-	});
+}
+
+const seedFromDisk = (name) => {
+		return new Promise(function(resolve, reject) {
+			
+			let path = "./"+name+"/databox-manifest.json";
+			fs.readFile(path,'utf8', (error,body)=>{
+				if(error) {
+					console.log("[seeding manifest] Failed to get manifest from " + path, error);
+				}
+				req.post({
+						uri: "http://" + Config.localAppStoreName + "/app/post",
+						method: "POST",
+						form: {"manifest": body}
+						}, (error,response,body) => {
+							if(error) {
+								console.log("[seeding manifest] Failed to POST manifest " + url, error);
+							} 
+							resolve();
+						});
+			});
+		});	
+}
+
+const req = request.defaults({jar: true});
+wait()
+.then(()=>{
+		req.get("http://" + Config.localAppStoreName,(error,response,body)=>{
+			if(error) {
+				console.log("[seeding manifest] get app store root to log in", error);
+				return Promise.reject();
+			}
+			
+			if(process.env.DATABOX_DEV == 1) {
+				let fn = seedFromDisk(req);
+			} else {
+				let fn = seedFromGithub(req);
+			}
+			let proms = Config.localAppStoreSeedManifests.map(fn)
+			return Promise.all(proms);
+		});
+})
+.catch(err => {
+	console.log(err);
+	const stack = new Error().stack;
+	console.log(stack);
+});
+
+
 
--- startDatabox.sh
+++ startDatabox.sh
@@ -5,22 +5,52 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
+DEV=""
+if [ "$1" == "dev" ]
+then
+    DEV=1
+    export DATABOX_DEV="1"
+else
+  DEV=0 
+  export DATABOX_DEV="0"
+fi
+
+if [ "$1" == "sdk" ]
+then
+    #enable SDK mode
+    export DATABOX_SDK="1"
+else 
+    export DATABOX_SDK="0"
+fi
+
 ARCH=$(uname -m)
 
 if [ "$ARCH" == 'armv7l' ]
 then
      NODE_IMAGE="hypriot/rpi-node:slim"
      export DATABOX_ARCH="-"${ARCH}
+     DEV=1 #ARM is only supported in dev mode with localy built images (for now)
+     export DATABOX_DEV="1"
 elif [ "$ARCH" == 'aarch64' ]
 then
      NODE_IMAGE="forumi0721alpineaarch64/alpine-aarch64-nodejs"
      export DATABOX_ARCH="-"${ARCH}
+     DEV=1 #ARM is only supported in dev mode with localy built images (for now)
+     export DATABOX_DEV="1"
 else
      ARCH=""
      NODE_IMAGE="node:alpine"
      export DATABOX_ARCH=""
 fi
 
+if [ "$DEV" != "1" ]
+then
+    #use images from https://hub.docker.com/r/databoxsystems/ 
+    export DOCKER_REPO="databoxsystems/"
+else
+    #use local images
+    export DOCKER_REPO=""   
+fi
 
 function dr () ( docker run --net=host -ti --rm -v "$(pwd -P)":/cwd -w /cwd $DARGS "$@" ;)
 function contNode { dr ${NODE_IMAGE}  "$@" ;}
@@ -30,7 +60,7 @@ if [ ! -d "node_modules" ]; then
     contNPM install
 fi
 
-./getCompnentSrc.sh
+
 
 docker swarm init
 
@@ -40,11 +70,20 @@ if [ ! -d "certs" ]; then
   contNode node ./src/createCerts.js
 fi
 
-docker-compose build
-docker-compose -f ./docker-compose-dev-local-images.yaml build
+
+if [ "$DEV" == "1" ]
+then
+  #only build local images in dev mode 
+  ./getCompnentSrc.sh
+  docker-compose build
+  docker-compose -f ./docker-compose-dev-local-images.yaml build
+fi
 
 docker stack deploy -c docker-compose.yaml databox
+
+DARGS="-e DATABOX_DEV=${DEV}"
 contNode node ./src/seedManifests.js
+
 
 echo "databox started goto http://127.0.0.1:8989"
 
