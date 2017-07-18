df697ee232f7c31c1031cc568d5931998a5ae665
ToshBrown <Toshbrown@gmail.com>
2017-07-18 16:05:39 +0100

    Removed unused docker commands

--- stopDatabox.sh
+++ stopDatabox.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 docker stack rm databox
 
-docker service rm $(docker service ls -q)
-
-docker secret rm $(docker secret ls -q)
-
 echo "waiting ....."
 sleep 10 #give docker some time to remove the networks etc
 docker swarm leave --force
