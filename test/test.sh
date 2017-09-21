53c9515a977048ecce796f482db82255e5d2cb2f
ToshBrown <Toshbrown@gmail.com>
2017-09-21 11:24:47 +0100

    Test if the arbitor is working

--- test/test.sh
+++ test/test.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m' 
+NC='\033[0m'
+
+function fail {
+    echo -e "[${RED}FAILED${NC}] ${1}"
+    #exit 1
+}
+
+function sucsess {
+    echo -e "[${GREEN}OK${NC}] $1"
+}
+
+function assert {
+  if [ "$1" != "$2" ]
+  then
+    fail "$3"
+  else
+    sucsess "$3"
+  fi
+}
+
+#Start databox
 export DATABOX_TESTING=1 
-source ./databox-start dev 
+#source ./databox-start dev 
 
-sleep 10
+sleep 5
 
+#can we see the CM UI
 STATUS=$(curl -sL -w "%{http_code}\\n" "http://127.0.0.1:8989/" -o /dev/null)
+assert $STATUS 200 "Is the Container Manager is avalable?"
 
-if [ $STATUS == 200 ]
-then 
-    exit 0
-else
-    exit 1
-fi
+#is the arbitor up 
+RES=$(curl -sL  "http://127.0.0.1:8989/arbiter/status")
+assert "$RES" "active" "Is the Arbitor is avalable?"
+
+#is the arbitor /cat endpoint ok 
+EXPECTED='{"catalogue-metadata":[{"rel":"urn:X-hypercat:rels:isContentType","val":"application/vnd.hypercat.catalogue+json"},{"rel":"urn:X-hypercat:rels:hasDescription:en","val":"Databox Root Catalogue"},{"rel":"urn:X-hypercat:rels:hasHomepage","val":"http://www.databoxproject.uk/"},{"rel":"urn:X-hypercat:rels:containsContentType","val":"application/vnd.hypercat.catalogue+json"},{"rel":"urn:Xhypercat:rels:supportsSearch","val":"urn:X-hypercat:search:simple"}],"items":[]}'
+RES=$(curl -sL  "http://127.0.0.1:8989/arbiter/cat")
+assert "$RES" "$EXPECTED" "Is the Arbitor cat endpoint is ok."
+
+exit 0
